c472f82b6e2fa597ee76cadca28f6d4280909e37
petya <mityushin_petya@mail.ru>
2022-04-13 17:32:09 +0300

    add button add delete, add, edit

---
+++ ExamBilet/LargestNumOutstand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamBilet
+{
+    class LargestNumOutstand
+    {
+        public string TypeFurniture { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ ExamBilet/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamBilet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Database database = new Database();
+        public MainWindow()
+        {
+            InitializeComponent();
+            table.ItemsSource = Database.GetListOrderFurnt();
+        }
+
+        private void ClickBackorders(object sender, RoutedEventArgs e)
+        {
+            LargestNumberOutstandApp largestNumberOutstandApp = new LargestNumberOutstandApp();
+            largestNumberOutstandApp.Show();
+        }
+
+        private void ClickOpenSumRequests(object sender, RoutedEventArgs e)
+        {
+            SumRequestsPlanDates sumRequestsPlanDates = new SumRequestsPlanDates();
+            sumRequestsPlanDates.Show();
+        }
+        private void ClickAdd(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void ClickDelete(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
